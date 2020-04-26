d0fe9f631265308b3f591ad312e9ef52f8838412
Shivang Dave <daveshivang15@gmail.com>
2020-04-26 19:27:21 -0400

    fix no API errors..

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,35 @@ import jwt from 'jwt-simple';
 
 const BASE_URL = 'http://localhost:3000/api/v1'
 const all_users = BASE_URL + '/users'
-const single_user = all_users + '/9'
+const single_user = all_users + '/5'
 const secret = process.env.REACT_APP_SUPER_SECRET_USER_KEY
 
+const App = (props) => {
+  decode(getAllReq())
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
 const decode = (message) => {
-  message.then(msg => {
+  message
+  .then(msg => {
     const token = msg.message
-    const data = jwt.decode(token,secret)
+    const data = jwt.decode(token, secret)
     console.log(data)
   })
+  .catch(_ => console.log("API out of reach.."))
 }
 
 const getReq = () => {
   return fetch(single_user)
+  .then(res => res.json())
+  .catch(err => console.log(err))
+}
+
+const getAllReq = () => {
+  return fetch(all_users)
   .then(res => res.json())
   .catch(err => console.log(err))
 }
@@ -49,14 +65,5 @@ const delReq = () => {
   .then(res => res.json())
 }
 
-const App = (props) => {
-
-  decode(postReq())
-
-  return (
-    <div className="App">
-    </div>
-  );
-}
 
 export default App;
